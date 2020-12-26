b5d645dc0aa941371ac5419d776324cf611bd1c2
srishamanth <srishamanth@gmail.com>
2020-12-26 23:22:53 +0530

    Updated all pages

--- pages/NotFoundPage/index.js
+++ pages/NotFoundPage/index.js
@@ -1,26 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import NavigationBar from '../../components/organisms/Navigation Bar/navigation-bar.js';
 
 function NotFoundPage(props) {
-    useEffect(() => {
-        document.title =
-            'GoodWay Attestation - One stop solution for all your attestation & apostille, Banglaore, India';
-        document
-            .querySelector('meta[name="description"]')
-            .setAttribute(
-                'content',
-                'Goodway Attestation & Apostille Service in Bangalore for Birth & Marriage Certificate, Education & Commercial Documents. For Quick Service Call:9148889666'
-            );
-        document
-            .querySelector('meta[name="keywords"]')
-            .setAttribute(
-                'content',
-                'Certificate Attestation Agency in Bangalore, attestation services near me, hrd attestation for nurses in bangalore, uae attestation services in bangalore, hrd attestation bangalore karnataka, apostille services in bangalore, Apostille Certification Services in Bangalore, Apostille Attestation in Bangalore, apostille attestation fee bangalore'
-            );
-        document.querySelector('link[rel="canonical"]').setAttribute('href', 'https://www.goodwayattestation.com/');
-    }, []);
     return (
-        <div className="routes-not-found">
+        <div className={styles['routes-not-found']}>
             <NavigationBar showTrackStatusBtn={true} />
             <h4>
                 You seemed to have lost track.. This is certainly not where you wished to land. Otherwise, it's all here
